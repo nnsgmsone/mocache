da819620b6e3332c4b644431cf986e4566cec544
nnsgmsone <nnsmgsone@outlook.com>
2023-10-26 16:08:01 +0800

    Use mmap instead of go make

--- internal/manual/manual.go
+++ internal/manual/manual.go
@@ -4,9 +4,38 @@
 
 package manual
 
+import (
+	"syscall"
+	"unsafe"
+)
+
+var fd = -1
+
+// The go:linkname directives provides backdoor access to private functions in
+// the runtime. Below we're accessing the throw function.
+
+//go:linkname throw runtime.throw
+func throw(s string)
+
+// New allocates a slice of size n. The returned slice is from manually managed
+// memory and MUST be released by calling Free. Failure to do so will result in
+// a memory leak.
 func New(n int) []byte {
-	return make([]byte, n)
+	if n == 0 {
+		return make([]byte, 0)
+	}
+	r0, _, e1 := syscall.Syscall6(syscall.SYS_MMAP, 0, uintptr(rollup(n)), uintptr(syscall.PROT_READ|syscall.PROT_WRITE),
+		uintptr(syscall.MAP_ANON|syscall.MAP_PRIVATE), uintptr(fd), uintptr(0))
+	if e1 != 0 {
+		throw("out of memory")
+	}
+	return unsafe.Slice((*byte)(unsafe.Pointer(r0)), n)
 }
 
-func Free(_ []byte) {
+func Free(b []byte) {
+	syscall.Syscall(syscall.SYS_MUNMAP, uintptr(unsafe.Pointer(&b[0])), uintptr(cap(b)), 0)
+}
+
+func rollup(n int) int {
+	return (n + 4095) & (^4095)
 }
